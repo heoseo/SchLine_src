71545b74eb93735a59b360dd93b887e313c2ed21
= <hsyoon1799@naver.com>
2021-02-19 16:39:21 +0900

    admin 0219

--- main/java/admin/model/AdminAttendTemplateDAO.java
+++ main/java/admin/model/AdminAttendTemplateDAO.java
@@ -12,6 +12,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.PreparedStatementSetter;
 
+import schline.ClassDTO;
 import schline.UserVO;
 import schline.util.JdbcTemplateConst;
 
@@ -45,7 +46,7 @@ public class AdminAttendTemplateDAO {
 		return result;
 	}
 	
-	public ArrayList<AttendanceDTO> listPage(
+	public ArrayList<AttendanceDTO> userList(
 			Map<String, Object> map){
 		
 		boolean flag = false;
@@ -102,174 +103,45 @@ public class AdminAttendTemplateDAO {
 						AttendanceDTO.class));
 	}
 	
-//	
-//	// 조회수 증가
-//	public void updateHit(final String idx) {
-//		
-//		String sql = "UPDATE springboard SET "
-//					+ "		hits = hits + 1"
-//					+ "WHERE idx=? ";
-//		
-//		// 
-//		template.update(sql, new PreparedStatementSetter() {
-//			
-//			@Override
-//			public void setValues(PreparedStatement ps) throws SQLException {
-//
-//				ps.setInt(1, Integer.parseInt(idx));
-//			}
-//		});
-//	}
-//	
-//	
-//	// 게시물 상세보기
-//	public SpringBbsDTO view(String idx) {
-//		
-//		updateHit(idx);
-//		
-//		SpringBbsDTO dto = new SpringBbsDTO();
-//		String sql = "SELECT * FROM springboard "
-//				+ "	  WHERE idx="+idx;
-//		
-//		/*
-//		- queryForObject()는 반환결과가 0개이거나 2개 이상인 경우 예외가 발생하므로
-//			반드시 예외처리를 해주는 것이 좋다.
-//		- 아래 try문안의 문장을 try문 밖에서 실행해주면 오류남.  */
-//		try {
-//			// 반환값이 하나만 있는 select계열의 쿼리문 실행
-//			dto = template.queryForObject(sql, 
-//					new BeanPropertyRowMapper<SpringBbsDTO>(SpringBbsDTO.class));
-//		}
-//		catch(Exception e) {
-//			System.out.println("View()실행시 예외발생");
-//		}
-//		
-//		return dto;
-//	}
-//	
-//	
-//	public int password(String idx, String pass) {
-//		
-//		int retNum = 0;
-//		
-//		String sql = "SELECT * FROM springboard "
-//				+ "	  WHERE pass=" + pass+" AND idx=" + idx;
-//		
-//		try {
-//			SpringBbsDTO dto = template.queryForObject(sql, 
-//							new BeanPropertyRowMapper<SpringBbsDTO>(
-//									SpringBbsDTO.class));
-//			retNum = dto.getIdx();
-//		}
-//		catch(Exception e) {
-//			System.out.println("password() 예외발생");
-//		}
-//		
-//		return retNum;
-//	}
-//	
-//	
-//	
-//	public void edit(final SpringBbsDTO dto) {
-//		System.out.println("DAO edit진입");
-//		
-//		String sql = "	UPDATE springboard "
-//				+ "		SET name=?, title=?, contents=?"
-//				+ "		WHERE idx=? AND pass=?";
-//		
-//		
-//		template.update(sql, new PreparedStatementSetter() {
-//			
-//			@Override
-//			public void setValues(PreparedStatement ps) throws SQLException {
-//
-//				System.out.println("DAO edit > title: " + dto.getTitle());
-//				
-//				ps.setString(1, dto.getName());
-//				ps.setString(2, dto.getTitle());
-//				ps.setString(3, dto.getContents());
-//				ps.setInt(4, dto.getIdx());
-//				ps.setString(5, dto.getPass());
-//				
-//			}
-//		});
-//	}
-//	
-//	
-//	public void delete(final String idx, final String pass) {
-//		
-//		String sql = "	DELETE FROM springboard "
-//				+ "		WHERE idx=? AND pass=?";
-//		
-//		template.update(sql, new PreparedStatementSetter() {
-//			
-//			@Override
-//			public void setValues(PreparedStatement ps) throws SQLException {
-//				
-//				ps.setString(1, idx);
-//				ps.setString(2, pass);
-//			}
-//		});
-//	}
-//	
-//	
-//	// 답변글 입력
-//	public void reply(final SpringBbsDTO dto) {
-//		/*
-//		 * 답변글을 입력하기전 일괄 업데이트 처리
-//		 */
-//		replyPrevUpdate(dto.getBgroup(), dto.getBstep());
-//		
-//		// write와 다른 점은 bgroup에 기존게시물의 번호가 들어가는 것이다.
-//		String sql = "	INSERT INTO springboard "
-//					+ "		(idx, name, title, contents, pass, "
-//					+ "			bgroup, bstep, bindent) "
-//					+ "	VALUES "
-//					+ "		(springboard_seq.nextval, ?, ?, ?, ?, ?, ?, ? )";
-//		
-//		template.update(sql, new PreparedStatementSetter() {
-//			
-//			@Override
-//			public void setValues(PreparedStatement ps) throws SQLException {
-//
-//				ps.setString(1, dto.getName());
-//				ps.setString(2, dto.getTitle());
-//				ps.setString(3, dto.getContents());
-//				ps.setString(4, dto.getPass());
-//				//원본글의 group번호를 입력
-//				ps.setInt(5, dto.getBgroup());
-//				//원본글의 step, indent에 +1한 후 입력
-//				ps.setInt(6, dto.getBstep()+1);
-//				ps.setInt(7, dto.getBindent()+1);
-//			}
-//		});
-//	}
-//	
-//	
-//	/*
-//	 * 답변글을 입력하기 전 현재 step보다 큰 게시물들을  일괄적으로 step+1해서 뒤로 
-//	 * 밀어주는 작업을 진행한다.
-//	 */
-//	public void replyPrevUpdate(final int strGroup, final int strStep) {
-//		
-//		String sql = "	UPDATE springboard "
-//				+ "		SET bstep = bstep+1 "
-//				+ "		WHERE bgroup=? AND bstep>?";
-//		
-//		template.update(sql, new PreparedStatementSetter() {
-//			
-//			@Override
-//			public void setValues(PreparedStatement ps) throws SQLException {
-//				ps.setInt(1, strGroup);
-//				ps.setInt(2, strStep);
-//				
-//			}
-//		});
-//	}
-//	
-//	
 	
-//
-//	
-//
+	public ArrayList<ClassDTO> subjectList(
+			Map<String, Object> map){
+		
+		boolean flag = false;
+		int start = 0, end = 0;
+		if(map.get("start")!= null && map.get("end")!= null) {
+			start = Integer.parseInt(map.get("start").toString());
+			end = Integer.parseInt(map.get("end").toString());
+			flag = true;
+		}
+		
+		String searchColumn = (String) map.get("searchColumn");
+		
+		String sql = "	SELECT * "
+				+ "		FROM ( "
+				+ "			SELECT Tb.*, rownum rNum "
+				+ "			FROM ( "
+				+ "				SELECT subject_idx, subject_name, U.* "
+				+ "				FROM user_tb U, subject_tb S "
+				+ "				WHERE U.user_id = S.user_id "
+				+ "					AND authority = '"+map.get("searchColumn")+"' ";
+		
+		if(map.get("searchWord")!=null)
+		sql += " 					AND user_name LIKE '%"+map.get("searchWord")+"%' ";				
+		sql += " 			ORDER BY U.user_id ASC"
+		+"    				) Tb"
+		+"				)";
+		if(flag ==true)
+			sql +=" 	WHERE rNum BETWEEN "+start+" and "+end;
+		
+		
+		
+		
+		
+		return (ArrayList<ClassDTO>)
+				template.query(sql, 
+				new BeanPropertyRowMapper<ClassDTO>(
+						ClassDTO.class));
+	}
+
 }
--- main/java/admin/model/AttendListCommand.java
+++ main/java/admin/model/AttendListCommand.java
@@ -9,6 +9,8 @@ import org.springframework.ui.Model;
 
 import admin.AdminCommandImpl;
 import admin.model.AdminUserTemplateDAO;
+import schline.ClassDTO;
+import schline.GradeDTOImpl;
 import schline.UserVO;
 import schline.util.PagingUtil;
 import schline.util.PagingUtil_admin;
@@ -25,82 +27,46 @@ public class AttendListCommand implements AdminCommandImpl{
 		HttpServletRequest req = (HttpServletRequest)paramMap.get("req");
 		
 		// DAO객체 생성
-		AdminUserTemplateDAO dao = new AdminUserTemplateDAO();
+		AdminAttendTemplateDAO attendDAO= new AdminAttendTemplateDAO();
+		AdminClassTemplateDAO classDAO = new AdminClassTemplateDAO();
 		
 		// 검색어 관련 폼값 처리
-		String addQueryString = "";
 		String searchColumn = req.getParameter("searchColumn");
 		if(searchColumn == null) {
 			searchColumn = "PROFESSOR";
 			System.out.println("UserListController > searchColumn 안들어옴.");
 		}
-		String searchWord = req.getParameter("searchWord");
+		String searchSubject = req.getParameter("searchSubject");
+		String searchUser = req.getParameter("searchUser");
 		
 		
-		addQueryString = String.format("searchColumn=%s", searchColumn);
-		paramMap.put("searchColumn",searchColumn);
-				
-		if(searchWord != null) {
-			addQueryString += String.format("searchWord=%s", searchWord);
-			paramMap.put("searchUser", searchWord);
-		}
-		
-		// paramMap : list_flag, table, Word 
-		
-		// 전체 레코드 수 카운트하기
-		int totalRecordCount = dao.getTotalCount(paramMap);
-		
-		
-		/************************페이지 처리 start***************************/
-		// Environment 객체를통한 외부파일 읽기
-		int pageSize = 10;
-		int blockPage= 6;
-		
-		// 전체 페이지 수를 계산
-		int totalPage = (int)Math.ceil((double)totalRecordCount/pageSize);
-		
-		// 현재 페이지 번호. 첫 진입일 떄는 무조건 1페이지로 지정
-		int nowPage = req.getParameter("nowPage")==null ? 1 :
-					Integer.parseInt(req.getParameter("nowPage"));
-		
-		// 리스트에 출력할 게시물의 구간을 계산
-		int start = (nowPage-1) * pageSize + 1;
-		int end = nowPage * pageSize;
-		
-		paramMap.put("start", start);
-		paramMap.put("end", end);
-		
+		paramMap.put("searchSubject",searchSubject);
+		paramMap.put("searchUser",searchUser);
 				
 		
-		/************************페이지 처리 end***************************/
 		
 		
+		// 과목 나열
+		ArrayList<ClassDTO> subjectLists = classDAO.listPage(paramMap);
 		
-		ArrayList<Admin_UserVO> listRows = dao.listPage(paramMap);
+		// 출석 나열
+		ArrayList<AttendanceDTO> userLists = attendDAO.userList(paramMap);
 		
-		// 가상번호 계산하여 부여하기
-		int virtualNum = 0;
-		int countNum = 0;
 		
-		System.out.println("UserListCommand > totalRecordCount = " + totalRecordCount );
-		for(Admin_UserVO row : listRows)
-		{
-			virtualNum = totalRecordCount - (((nowPage-1)*pageSize) + countNum++);
-			row.setVirtualNum(virtualNum);
-		}
-		 
-		// 리스트에 출력한 list컬렉션을 model객체에 저장한 후 뷰로 전달한다.
-		String pagingImg = PagingUtil_admin.pagingImg(totalRecordCount, pageSize, blockPage, nowPage, 
-											req.getContextPath()+"/admin/userList?"+addQueryString);
-		
-		model.addAttribute("pagingImg", pagingImg);
-		model.addAttribute("totalPage", totalPage);
-		model.addAttribute("nowPage", nowPage);
-		model.addAttribute("listRows", listRows);
+		model.addAttribute("subjectLists", subjectLists);
+		model.addAttribute("userLists", userLists);
 		model.addAttribute("paramMap", paramMap);
 		
-		// JDBCTemplete에서는 자원반납을 하지 않는다.
-		//dao.close();
+		
+		
+		
+
+		
+		
+		
+		
+		
+		//출석
 		
 	}
 }
